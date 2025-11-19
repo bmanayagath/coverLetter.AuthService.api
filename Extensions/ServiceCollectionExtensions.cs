ba328f783c22bfabfee1095da12e7aaa6427ed70
bijum777 <bijum777@gmail.com>
2025-11-19 11:35:56 +0300

    Updated the jwt settings

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -5,12 +5,20 @@ using coverLetter.AuthService.api.Data;
 using coverLetter.AuthService.api.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
 using System.Text;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddAuthServices(this IServiceCollection services, string connectionString)
+    public static IServiceCollection AddAuthServices(this IServiceCollection services, IConfiguration configuration)
     {
+        // Read connection string and JWT settings from appsettings
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var jwtSection = configuration.GetSection("Jwt");
+        var issuer = jwtSection["Issuer"];
+        var audience = jwtSection["Audience"];
+        var key = jwtSection["Key"];
+
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(connectionString));
 
@@ -18,21 +26,24 @@ public static class ServiceCollectionExtensions
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
 
-        services.AddAuthentication()
-            .AddJwtBearer(options =>
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
             {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    // Set your issuer and audience here
-                    ValidIssuer = "yourIssuer",
-                    ValidAudience = "yourAudience",
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("yourSecretKey"))
-                };
-            });
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = issuer,
+                ValidAudience = audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+            };
+        });
 
         return services;
     }
